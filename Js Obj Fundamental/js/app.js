6d7347940ba28be6a5ccd0dce1f27de0d908cc42
Diyorbek [FE16] <115080862+diyoruz206@users.noreply.github.com>
2022-11-23 13:55:15 +0500

    Add files via upload

---
+++ Js Obj Fundamental/js/app.js
@@ -0,0 +1,16 @@
+  // 1-HomeWork
+let obj = {
+    name: "Diyorbek",
+    surname: "Karomov ",
+    education:"Open  Web Academy",
+    age: 16,
+    birthdaYears: 2006,
+
+};
+console.log(obj);
+
+
+// 2-HomeWork
+let num1 = 30;
+let num2 = 9;
+console.log(Number(num1) % Number(num2));
